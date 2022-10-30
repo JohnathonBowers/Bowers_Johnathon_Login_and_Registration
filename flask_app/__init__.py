f063d9eb6544dde20e94b9dea9bb1f5510cd9b01
Johnathon Bowers <johnathonbowers@gmail.com>
2022-10-30 11:43:11 -0400

    set up initial code

---
+++ flask_app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.secret_key = "165c559ccc71c961aa31510d05b9f8a2"
---
+++ flask_app/models/user.py
@@ -0,0 +1,10 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class User:
+    def __init__ (self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
